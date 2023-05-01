de0d14f69266db8d48b465b87599df74d0749c62
MohonSaha <practicewithmohon@gmail.com>
2023-05-02 02:14:33 +0600

    implement footer

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>I am footer</h2>
-        </div>
+        <Container className=' mt-5'>
+            <div className='d-flex justify-content-between'>
+                <div className='d-flex flex-column'>
+                    <Link className='text-decoration-none text-dark'>Presentations</Link>
+                    <Link className='text-decoration-none text-dark'>Professionals</Link>
+                    <Link className='text-decoration-none text-dark'>Stores</Link>
+                </div>
+                <div className='d-flex flex-column'>
+                    <Link className='text-decoration-none text-dark'>Presentations</Link>
+                    <Link className='text-decoration-none text-dark'>Professionals</Link>
+                    <Link className='text-decoration-none text-dark'>Stores</Link>
+                </div>
+                <div className='d-flex flex-column'>
+                    <Link className='text-decoration-none text-dark'>Contact Form</Link>
+                    <Link className='text-decoration-none text-dark'>Work With Us</Link>
+                    <Link className='text-decoration-none text-dark'>Visit Us</Link>
+                </div>
+
+                <div className='d-flex flex-column'>
+                    <Link className='text-decoration-none text-dark'><FaFacebook /> Facebook</Link>
+                    <Link className='text-decoration-none text-dark'><FaInstagram /> Instagram</Link>
+                    <Link className='text-decoration-none text-dark'><FaTwitter /> Twitter</Link>
+                </div>
+            </div>
+
+            <p className='text-center mt-3 mb-0 pb-2'><small>All Rights Resserved By Cooking Master</small></p>
+
+        </Container>
+
     );
 };
 
