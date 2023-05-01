15ac16e743f40e4c99fc388071aac4c9f90d30be
MohonSaha <practicewithmohon@gmail.com>
2023-05-02 01:43:04 +0600

    implement banner

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,11 +3,13 @@ import Header from '../pages/Shared/Header/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
 import Navbar from '../pages/Shared/Header/Navbar';
+import Banner from '../pages/Home/Banner/Banner';
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner></Banner>
             <Outlet></Outlet>
             <Footer></Footer>
             
---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import banner from '../../../assets/banners/banner.jpg'
+import { Container, Image } from 'react-bootstrap';
+
+const Banner = () => {
+    return (
+        <Container className='mt-5'>
+            <Image className='rounded-3 position-relative' style={{height:'400px', width: '100%'}} src={banner}/>
+
+            <div style={{width: '600px', bottom: '280px'}} className='position-absolute start-50 me-5 pe-5'>
+                <h2 className='text-light fs-1'>Lorem ipsum dolor sit amet consectetur adipisicing.</h2>
+                <p className='text-light'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro incidunt beatae, unde accusantium repudiandae voluptatibus?</p>
+            </div>
+        </Container>
+    );
+};
+
+export default Banner;
