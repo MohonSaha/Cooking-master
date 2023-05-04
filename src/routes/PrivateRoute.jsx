078a6f8e2a2cb50f6155db40d14e6f97bb699142
MohonSaha <practicewithmohon@gmail.com>
2023-05-04 10:35:03 +0600

    responsive spinner

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,19 +3,19 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProviders';
 import Spinner from 'react-bootstrap/Spinner';
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
 
-    const {user, loading} = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
 
-    if(loading){
-        return <Spinner animation="border" variant="primary" />
+    if (loading) {
+        return <Spinner className='d-flex align-items-center justify-content-center mx-auto' style={{}} animation="border" variant="primary" />
     }
 
-    if(user){
+    if (user) {
         return children
     }
-    return <Navigate state={{from: location}} to="/login" replace></Navigate>
+    return <Navigate state={{ from: location }} to="/login" replace></Navigate>
 };
 
 export default PrivateRoute;
