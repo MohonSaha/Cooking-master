6792c5af7c27188e589e4ae89369c91679ef8fca
MohonSaha <practicewithmohon@gmail.com>
2023-05-04 18:40:28 +0600

    design banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import banner from '../../../assets/banners/banner.jpg'
 import { Container, Image } from 'react-bootstrap';
 import './Banner.css'
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
 
 const Banner = () => {
     return (
-        <Container className='mt-5'>
-            <Image className='rounded-3 carousel-inner' style={{height:'500px', width: '100%'}} src={banner}/>
+        <Container>
+            <div className='mt-5 carousel-inner'>
+                <Image className='rounded-3 ' style={{ height: '500px', width: '100%' }} src={banner} />
 
-            <div style={{width: '600px', bottom: '280px'}} className=' banner-container'>
-                <h2 className='text-light fs-1 w-75'>Choose from thousands of recipes</h2>
-                <p className='text-light'>Good recipes services prioritize customer experience by offering user-friendly interfaces, personalized recommendations, and the ability to save favorite recipes. They also often provide community features, such as user reviews and ratings.</p>
+                <div style={{ width: '600px', bottom: '280px' }} className=' banner-container'>
+                    <h2 className='text-light fs-1 w-75'>Choose from thousands of recipes</h2>
+                    <p className='text-light'>Good recipes services prioritize customer experience by offering user-friendly interfaces, personalized recommendations, and the ability to save favorite recipes. They also often provide community features, such as user reviews and ratings.</p>
+                    <p><Link to='blog' className='text-decoration-none text-light fw-bold fs-4'>Learn More <FaArrowRight></FaArrowRight></Link></p>
+                </div>
+
+
             </div>
-
         </Container>
     );
 };
